a1484cede87bfce1c9b8a92ecfe926e2f1f24b04
gzzchh <xjdzch@126.com>
2019-09-28 01:07:05 +0800

    修正排版

--- src/Cmds/VM/VM.php
+++ src/Cmds/VM/VM.php
@@ -32,7 +32,8 @@ class  VM
 
         // 失败处理
         if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
+//            throw new ProcessFailedException($process);
+            return $process->getErrorOutput();
         }
 
         return $process->getOutput();
@@ -80,48 +81,62 @@ class  VM
     public function change($annotation, $cpuHotAdd, $cpuLimit, $cpuPerformanceCounter, $cpuReservation, $cpus, $cpuShares, $guestOS, $memoryLimit, $memory, $memoryHotAdd, $memoryShare, $nestedHvEnabled, $syncTimeWithHost, $vm, array $extraConfig)
     {
         $cmd = [$this->GovcBin, 'vm.change', '-vm', $vm];
+
         // 虚拟机备注
         if ($annotation != null) {
             // -annotation string
             $annotationParameter = ['-annotation', $annotation];
             array_merge($cmd, $annotationParameter);
         }
+
         // CPU热添加
         if ($cpuHotAdd != null) {
             // -cpu-hot-add-enabled bool
             $cpuHotAddParameter = ['-cpu-hot-add-enabled', $cpuHotAdd];
             array_merge($cmd, $cpuHotAddParameter);
         }
+
         // CPU资源限制方面不填写 则设置为 -1 代表不受限制
         if ($cpuLimit != null) {
             // -cpu.limit int
             $cpuLimitParameter = ['-cpu.limit', $cpuLimit];
             array_merge($cmd, $cpuLimitParameter);
         }
+
         // CPU虚拟化计数器
         if ($cpuPerformanceCounter != null) {
             // -vpmc-enabled bool
             $cpuPerformanceCounterParameter = ['-vpmc-enabled', $cpuPerformanceCounter];
             array_merge($cmd, $cpuPerformanceCounterParameter);
         }
+
         // CPU保留 最少为 0
         if ($cpuReservation != null) {
             // -cpu.reservation int
             $cpuReservationParameter = ['-cpu.reservation', $cpuReservation];
             array_merge($cmd, $cpuReservationParameter);
         }
+
         // CPU核心数
         if ($cpus != null) {
             // -c int
             $cpusParameter = ['-c', $cpus];
             array_merge($cmd, $cpusParameter);
         }
+
         // CPU份额
         if ($cpuShares != null) {
             // -cpu.shares {normal,high,low}
             // -cpu.shares int
             $cpuSharesParameter = ['-cpu.shares', $cpuShares];
             array_merge($cmd, $cpuSharesParameter);
+        }
+        
+        // GuestOS 客户机系统类型
+        if ($guestOS != null) {
+            // -g string
+            $guestOSParameter = ['-g', $guestOS];
+            array_merge($cmd, $guestOSParameter);
         }
     }
 }
